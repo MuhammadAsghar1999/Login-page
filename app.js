8a08b137939452fe78fa759950620b49bb1ad54d
MuhammadAsghar1999 <143824430+MuhammadAsghar1999@users.noreply.github.com>
2023-11-10 00:26:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,140 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAfYMOR3c12Io1xoybxGFv-FoUwfN4MhNQ",
+  authDomain: "loginpage-eb3ef.firebaseapp.com",
+  projectId: "loginpage-eb3ef",
+  storageBucket: "loginpage-eb3ef.appspot.com",
+  messagingSenderId: "37093394583",
+  appId: "1:37093394583:web:ee5ed86f6f1807ed1981d2"
+};
+
+// Initialize Firebase
+var app = firebase.initializeApp(firebaseConfig);  
+ // *********************Sign Up Auth**********************
+ 
+ function signUp() {
+   var email = document.getElementById("email");
+   var password = document.getElementById("password");
+ 
+   firebase
+     .auth()
+     .createUserWithEmailAndPassword(email.value, password.value)
+     .then((userCredential) => {
+       // Signed in
+       var user = userCredential.user;
+       console.log(user);
+       firebase
+         .auth()
+         .currentUser.sendEmailVerification()
+         .then(() => {
+           alert("email verification sent..");
+         });
+     })
+     .catch((error) => {
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       console.log(errorMessage);
+     });
+ }
+ 
+ // *********************Login Auth**********************
+ 
+ function login() {
+   var email = document.getElementById("email");
+   var password = document.getElementById("password");
+ 
+   firebase
+     .auth()
+     .signInWithEmailAndPassword(email.value, password.value)
+     .then((userCredential) => {
+       // Signed in
+       var user = userCredential.user;
+       console.log(user);
+     })
+     .catch((error) => {
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       console.log(errorMessage);
+     });
+ }
+ 
+ // *********************Forgot Password Auth**********************
+ 
+ function forgotPassword() {
+   var email = document.getElementById("email");
+ 
+   firebase
+     .auth()
+     .sendPasswordResetEmail(email.value)
+     .then(() => {
+       alert("password reset email sent..");
+     })
+     .catch((error) => {
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       console.log(errorMessage);
+     });
+ }
+ 
+ // *********************Google Auth**********************
+ 
+ function loginWithGoogle(){
+   var provider = new firebase.auth.GoogleAuthProvider();
+   firebase
+     .auth()
+     .signInWithPopup(provider)
+     .then((result) => {
+       /** @type {firebase.auth.OAuthCredential} */
+       var credential = result.credential;
+ 
+       // This gives you a Google Access Token. You can use it to access the Google API.
+       var token = credential.accessToken;
+       // The signed-in user info.
+       var user = result.user;
+       console.log(user);
+     })
+     .catch((error) => {
+       // Handle Errors here.
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       console.log(errorMessage);
+       // The email of the user's account used.
+       var email = error.email;
+       // The firebase.auth.AuthCredential type that was used.
+       var credential = error.credential;
+       // ...
+     });
+ }
+ 
+ // *********************Github Auth**********************
+ 
+ function loginWithGithub() {
+   var provider = new firebase.auth.GithubAuthProvider();
+ 
+   firebase
+     .auth()
+     .signInWithPopup(provider)
+     .then((result) => {
+       /** @type {firebase.auth.OAuthCredential} */
+       var credential = result.credential;
+ 
+       // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+       var token = credential.accessToken;
+ 
+       // The signed-in user info.
+       var user = result.user;
+       console.log(user);
+       // IdP data available in result.additionalUserInfo.profile.
+       // ...
+     })
+     .catch((error) => {
+       // Handle Errors here.
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       console.log(errorMessage);
+       // The email of the user's account used.
+       var email = error.email;
+       // The firebase.auth.AuthCredential type that was used.
+       var credential = error.credential;
+       // ...
+     });
+ }
